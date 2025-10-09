193bf5cc4a30610ecc3d28a097c013722b54f7e2
Moises-Silva-Dev <moises.gonzalez.dev@gmail.com>
2025-10-09 17:22:29 -0600

    vista principal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Sitio de Soporte Técnico cargado.");
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+
+    // Add 'header-scrolled' class if user scrolls more than 50px, else remove it
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
